3e5e62c669fc7c41b3357fb66db3da20c6088dba
Felia <felia.14117072@student.itera.ac.id>
2019-11-14 00:11:30 +0700

    Praktikum minggu ke-9

---
+++ Week 9/Latihan/2/tes_cookies.php
@@ -0,0 +1,20 @@
+<?php setcookie("test_cookie", "test", time() + 3600, '/'); ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Test Cookies</title>
+</head>
+<body>
+	<?php
+	if (count($_COOKIE) > 0) {
+		echo "Cookies enabled.";
+	}else {
+		echo "Cookies disabled.";
+	}
+	?>
+	<!-- Ketika membuka file tes_cookies.php dengan browser incognito yang dihasilkan adalah "Cookies disabled." karena pada
+    browser incognito tidak menyimpan informasi ketika membuka file tersebut seperti cache, cookies, password, dan histori sedangkan
+    ketika membuka file tes_cookies.php dengan browser tanpa incognito yang dihasilkan adalah "Cookies enabled." karena browser
+    menyimpan informasi -->
+</body>
+</html>
